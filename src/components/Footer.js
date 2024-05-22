2bf8cb4647e14c6af7b2d217d107ef47b86efe0c
computerSaysNo2112 <Lovishhgz@gmail.com>
2024-05-21 20:08:12 -0400

    first commit

---
+++ src/components/Footer.js
@@ -0,0 +1,40 @@
+import React from "react";
+import { pageLinks, socialLinks } from "../data";
+
+export const Footer = () => {
+  return (
+    <footer className="section footer">
+      <ul className="footer-links">
+        {pageLinks.map((link) => {
+          const { id, href, text } = link;
+          return (
+            <li key={id}>
+              <a href={href} className={"footer-link"}>
+                {text}
+              </a>
+            </li>
+          );
+        })}
+      </ul>
+
+      <ul className="footer-icons">
+        {socialLinks.map((social) => {
+          const { id, href, icon } = social;
+          return (
+            <li key={id}>
+              <a href="blank" target={id} className="footer-icon">
+                <i className={icon}></i>
+              </a>
+            </li>
+          );
+        })}
+      </ul>
+      <p className="copyright">
+        copyright &copy; Backroads travel tours company
+        <span id="date">{new Date().getUTCFullYear()}</span> all rights reserved
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
